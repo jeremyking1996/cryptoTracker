ba4cdc0089daf48a122c9a10fd281aa3db4ec619
Jeremy King <jeremy.david.king.1996@gmail.com>
2021-08-25 15:50:44 -0400

    finished crypto tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import Coin from './Coin'
+
+const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios.get(url).then(res => {
+      setCoins(res.data);
+      console.log(res.data);
+    }).catch(error => console.log(error))
+  }, []);
+  
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+
+  const filteredCoins = coins.filter(coin => 
+    coin.name.toLowerCase().includes(search.toLowerCase()
+  ))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency!</h1>
+        <form>
+          <input type="text" placeholder="search"
+           className="coin-input" onChange={handleChange} />
+        </form>
+      </div>
+    {filteredCoins.map(coin => {
+      return(
+        <Coin 
+        key={coin.id} 
+        name={coin.name} 
+        image={coin.image} 
+        symbol={coin.symbol}
+        volume={coin.market_cap}
+        price={coin.current_price}
+        priceChange={coin.price_change_percentage_24h}
+        marketcap={coin.total_volume}
+        />
+      )
+    })}
     </div>
   );
 }
